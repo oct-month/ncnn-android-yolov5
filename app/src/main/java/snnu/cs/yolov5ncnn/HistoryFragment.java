52c2db7035f7f01f9d28021fa4e351a723aae71e
sun <ablocker@163.com>
2022-03-21 00:34:28 +0800

    完成删除功能

--- app/src/main/java/snnu/cs/yolov5ncnn/HistoryFragment.java
+++ app/src/main/java/snnu/cs/yolov5ncnn/HistoryFragment.java
@@ -1,5 +1,7 @@
 package snnu.cs.yolov5ncnn;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +9,14 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.OrientationHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -42,11 +44,7 @@ public class HistoryFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
-        RecyclerView imageListView = view.findViewById(R.id.image_list_view);
-        LinearLayoutManager layoutManager = new GridLayoutManager(view.getContext(), 2, GridLayoutManager.VERTICAL, false);
-        imageListView.setLayoutManager(layoutManager);
-        // layoutManager.setOrientation(RecyclerView.VERTICAL);
-        imageListView.setAdapter(new ImageListAdapter(getContext().getCacheDir().list(new FilenameFilter() {
+        FilenameFilter imgFilter = new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
                 if (name.endsWith(".jpg")) {
@@ -54,10 +52,39 @@ public class HistoryFragment extends Fragment
                 }
                 return false;
             }
-        })));
+        };
+
+        RecyclerView imageListView = view.findViewById(R.id.image_list_view);
+        LinearLayoutManager layoutManager = new GridLayoutManager(view.getContext(), 2, GridLayoutManager.VERTICAL, false);
+        imageListView.setLayoutManager(layoutManager);
+        // layoutManager.setOrientation(RecyclerView.VERTICAL);
+        ImageListAdapter imageListAdapter = new ImageListAdapter(getContext().getCacheDir().list(imgFilter));
+        imageListView.setAdapter(imageListAdapter);
         imageListView.addItemDecoration(new DividerItemDecoration(view.getContext(), RecyclerView.HORIZONTAL));
         imageListView.addItemDecoration(new DividerItemDecoration(view.getContext(), RecyclerView.VERTICAL));
         imageListView.setItemAnimator(new DefaultItemAnimator());
+
+        FloatingActionButton deleteImgBtn = view.findViewById(R.id.delete_img_btn);
+        deleteImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+                dialog.setMessage("删除所有图片？");
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        imageListAdapter.clearAllImg();
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                dialog.show();
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/snnu/cs/yolov5ncnn/ImageListAdapter.java
+++ app/src/main/java/snnu/cs/yolov5ncnn/ImageListAdapter.java
@@ -7,57 +7,88 @@ import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ViewHolder>
 {
-    private String[] localDataSets;
+    private List<String> localDataSets;
+    private Context mcontext;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder
     {
-        private final ImageView imageView;
-        private final Context mcontext;
+        private final CardView historyCard;
+        private final ImageView historyImg;
+        private final Button historyBtn;
 
-        public ViewHolder(View view, Context context)
+        public ViewHolder(View view)
         {
             super(view);
-            imageView = view.findViewById(R.id.history_img);
-            mcontext = context;
+            historyCard = view.findViewById(R.id.history_card);
+            historyImg = view.findViewById(R.id.history_img);
+            historyBtn = view.findViewById(R.id.history_btn);
+
+            historyCard.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    historyBtn.setVisibility(View.VISIBLE);
+                    return true;
+                }
+            });
+            historyBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    File file = new File(mcontext.getCacheDir(), localDataSets.get(position));
+                    if (file.delete()) {
+                        Toast.makeText(mcontext, "图片已删除", Toast.LENGTH_SHORT).show();
+                        localDataSets.remove(position);
+                        notifyItemRemoved(position);
+                    }
+                }
+            });
         }
 
         public ImageView getImageView()
         {
-            return imageView;
+            return historyImg;
         }
     }
 
     public ImageListAdapter(String[] dataSet)
     {
-        localDataSets = dataSet;
+        localDataSets = new LinkedList<>(Arrays.asList(dataSet));
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
+        this.mcontext = parent.getContext();
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_row_item, parent, false);
-        return new ViewHolder(view, parent.getContext());
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position)
     {
-        String filename = localDataSets[position];
+        String filename = localDataSets.get(position);
         try {
-            File file = new File(holder.mcontext.getCacheDir(), filename);
+            File file = new File(mcontext.getCacheDir(), filename);
             FileInputStream in = new FileInputStream(file);
             Bitmap bitmap = BitmapFactory.decodeStream(in);
             holder.getImageView().setImageBitmap(bitmap);
@@ -70,6 +101,19 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.View
     @Override
     public int getItemCount()
     {
-        return localDataSets.length;
+        return localDataSets.size();
+    }
+
+    public void clearAllImg()
+    {
+        String[] filenames = localDataSets.toArray(new String[0]);
+        for (int i = filenames.length - 1; i >= 0; -- i) {
+            File f = new File(mcontext.getCacheDir(), filenames[i]);
+            if (f.delete()) {
+                localDataSets.remove(i);
+            }
+        }
+        // localDataSets.clear();
+        notifyDataSetChanged();
     }
 }
